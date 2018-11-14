f22cd88e9636e2a61cb6e97f3fbfa72813e73e37
Nathan Miranda <nate845@gmail.com>
2018-11-14 23:14:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function fetchBooks() {
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
+    if (book.title)
+    
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
